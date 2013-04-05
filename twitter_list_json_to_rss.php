ae388004386be57a360aec4cfd579fa4e2a8ecd5
Thomas Yung <thomas.yung@roch.edu>
2013-04-05 12:35:44 -0500

    Made changes as per W3C RSS Feed Validator.
    The feed did not validate properly, so I made changes as per W3C RSS
    Feed Validator suggestions.

--- twitter_list_json_to_rss.php
+++ twitter_list_json_to_rss.php
@@ -1,28 +1,28 @@
 <?php
-	$script_name = 'twitter_list_json_to_rss.php';
+	header("Content-Type: application/xml; charset=UTF-8");
 	$screen_name = $_GET['screen_name'];
 	$list_name = $_GET['list_name'];
 	$statuses_url = 'http://api.twitter.com/1/'.$screen_name.'/lists/'.$list_name.'/statuses.json?per_page=100';
 	$fetch_json = file_get_contents($statuses_url);
 	$return = json_decode($fetch_json);
 	$now = date("D, d M Y H:i:s T");
-	$output = "<?xml version=\"1.0\"?>
-		<rss version=\"2.0\">
-			<channel>
-				<title>".$list_name."</title>
-				<link>http://".$_SERVER['SERVER_NAME']."/".$script_name."?screen_name=".$screen_name."&list_name=".$list_name."</link>
-				<description>".$list_name."</description>
-				<pubDate>$now</pubDate>
-				<lastBuildDate>$now</lastBuildDate>
-				";
-	foreach ($return as $line){
-		$output .= "<item><title>".htmlentities($line->user->screen_name.": ".$line->text)."</title>
-			<link>".htmlentities("https://twitter.com/".$line->user->screen_name."/statuses/".$line->id_str)."</link>
-			<description>".htmlentities(strip_tags($line->text))."</description>
-			<author>".$line->user->screen_name."</author>
-			</item>";
-	}
-	$output .= "</channel></rss>";
-	header("Content-Type: application/rss+xml");
-	echo $output;
+	$link = htmlspecialchars('http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'].'?'.$_SERVER['QUERY_STRING']);
 ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title><?php echo $list_name; ?></title>
+		<link><?php echo $link; ?></link>
+		<atom:link href="<?php echo $link; ?>" rel="self" type="application/rss+xml" />
+		<description><?php echo $list_name; ?></description>
+		<pubDate><?php echo $now; ?></pubDate>
+		<lastBuildDate><?php echo $now; ?></lastBuildDate>
+	<?php foreach ($return as $line){ ?>
+	<item>
+		<title><?php echo htmlspecialchars(htmlspecialchars_decode($line->user->screen_name.": ".strip_tags($line->text))); ?></title>
+		<description><?php echo htmlspecialchars(htmlspecialchars_decode(strip_tags($line->text))); ?></description>
+		<guid><?php echo htmlspecialchars("https://twitter.com/".$line->user->screen_name."/statuses/".$line->id_str); ?></guid>
+		<link><?php echo htmlspecialchars("https://twitter.com/".$line->user->screen_name."/statuses/".$line->id_str); ?></link>
+	</item>
+	<?php } ?>
+</channel>
+</rss>
